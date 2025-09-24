a86dfb2745f5de49f8b5086c8405373fbc1ff4b3
Adrian Bartholomew <adrian@bartholomusic.com>
2025-09-24 07:29:27 -0500

    fix type errors

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -5,10 +5,13 @@ import CalForm from "./CalForm";
 import Header from "@/components/header";
 import Hero from "@/components/hero";
 import Footer from "@/components/footer";
+import { getNavigationLinks } from "@/utils/constants";
+
+const navigationLinks = getNavigationLinks("");
 
 const Calendar: FC = () => (
   <main className="bg-bg-dark text-white text-base">
-    <Header />
+    <Header navigationLinks={navigationLinks} />
     <Hero basic={true} />
     <CalForm />
     <Footer />
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,10 +5,13 @@ import Header from "@/components/header";
 import Hero from "@/components/hero";
 import LoginForm from "@/app/login/login-form";
 import Footer from "@/components/footer";
+import { getNavigationLinks } from "@/utils/constants";
+
+const navigationLinks = getNavigationLinks("");
 
 const Login: FC = () => (
   <main className="bg-bg-dark text-white text-base">
-    <Header />
+    <Header navigationLinks={navigationLinks} />
     <Hero basic={true} />
     <LoginForm />
     <Footer />
